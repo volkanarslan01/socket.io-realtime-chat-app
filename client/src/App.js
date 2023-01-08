98ca63f9c23586c23525418d4250093ee7a6a2ff
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2023-01-08 16:36:36 +0300

    Connect socket io and data transfer

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+import Chat from "./Chat";
+const socket = io.connect("http://localhost:3006");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Join A Chat </h3>
+      <input
+        type="text"
+        placeholder="Volkan"
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Room ID "
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join A Room </button>
+      <Chat socket={socket} username={username} room={room} />
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -10,19 +10,26 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`USER with ID: ${socket.id} joined room ${data}`);
+  });
+  socket.on("send_message", (data) => {
+    console.log(data);
+  });
   io.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
 });
 
-server.listen(3001, () => {
-  console.log("Listenin in port 3001");
+server.listen(3006, () => {
+  console.log("Listenin in port 3006");
 });
